20d194cfa8896a7cfe98db1a893a179b652716e5
Mariano <marianotoppino@gmail.com>
2023-04-22 00:54:15 -0300

    First three fragments created

---
+++ app/src/main/java/com/example/fitnessapp/fragments/NutricionFragment.kt
@@ -0,0 +1,32 @@
+package com.example.fitnessapp.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fitnessapp.R
+
+class NutricionFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NutricionFragment()
+    }
+
+    private lateinit var viewModel: NutricionViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_nutricion, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NutricionViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/fitnessapp/fragments/NutricionViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fitnessapp.fragments
+
+import androidx.lifecycle.ViewModel
+
+class NutricionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
