0947b3d7b8e2f524b633f18dfc794999ccf1b69d
Luciano Gobo <rusogobo@gmail.com>
2023-04-22 05:30:23 -0300

    Paso de rutinaAlimenticia a nutricion

--- app/src/main/java/com/example/fitnessapp/fragments/NutricionHomeFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragments/NutricionHomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.fitnessapp.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,17 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.fitnessapp.R
 
-class RutinaAlimenticiaHomeFragment : Fragment() {
+class NutricionHomeFragment : Fragment() {
 
     lateinit var v : View
 
-    private lateinit var viewModel: RutinaAlimenticiaHomeViewModel
+    private lateinit var viewModel: NutricionHomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        v = inflater.inflate(R.layout.fragment_rutina_alimenticia_home, container, false)
+        v = inflater.inflate(R.layout.fragment_nutricion_home, container, false)
         return v
     }
 
--- app/src/main/java/com/example/fitnessapp/fragments/NutricionHomeViewModel.kt
+++ app/src/main/java/com/example/fitnessapp/fragments/NutricionHomeViewModel.kt
@@ -2,6 +2,6 @@ package com.example.fitnessapp.fragments
 
 import androidx.lifecycle.ViewModel
 
-class RutinaAlimenticiaHomeViewModel : ViewModel() {
+class NutricionHomeViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
