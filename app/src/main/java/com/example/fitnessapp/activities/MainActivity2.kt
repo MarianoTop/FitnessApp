ee3de21c2e5eb5d3d2c755a1b5469f4b21530dec
Luciano Gobo <rusogobo@gmail.com>
2023-04-22 15:29:40 -0300

    La bottom_bar ya funciona

--- app/src/main/java/com/example/fitnessapp/activities/MainActivity2.kt
+++ app/src/main/java/com/example/fitnessapp/activities/MainActivity2.kt
@@ -7,19 +7,17 @@ import androidx.navigation.ui.NavigationUI
 import com.example.fitnessapp.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var bottomNavView : BottomNavigationView
     private lateinit var navHostFragment : NavHostFragment
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost2) as NavHostFragment
-        bottomNavView = findViewById(R.id.bottom_bar)
+        bottomNavView = findViewById(R.id.bottomNavigationView)
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
     }
