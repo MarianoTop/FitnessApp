1959dcca69ec9bb91fe91045299a470c327da3df
Mariano <marianotoppino@gmail.com>
2023-04-24 00:31:39 -0300

    Se eliminan algunos comentarios en la pantalla de previsualizacion

--- app/src/main/java/com/example/fitnessapp/fragments/PrevisualizacionEjercicioFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragments/PrevisualizacionEjercicioFragment.kt
@@ -1,19 +1,21 @@
 package com.example.fitnessapp.fragments
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnessapp.R
-import com.example.fitnessapp.activities.MainActivity2
 import com.example.fitnessapp.adapters.EjercicioAdapter
 import com.example.fitnessapp.entities.EjercicioRepository
+
 
 class PrevisualizacionEjercicioFragment : Fragment() {
 
@@ -22,18 +24,6 @@ class PrevisualizacionEjercicioFragment : Fragment() {
     lateinit var recyclerEjercicios : RecyclerView
     lateinit var adapter: EjercicioAdapter
 
-    /*
-    lateinit var txtEjercicio1 : TextView
-    lateinit var txtEjercicio2 : TextView
-    lateinit var txtEjercicio3 : TextView
-    lateinit var txtEjercicio4 : TextView
-
-
-    lateinit var imagenEj1 : ImageView
-    lateinit var imagenEj2 : ImageView
-    lateinit var imagenEj3 : ImageView
-    lateinit var imagenEj4 : ImageView
-*/
 
     lateinit var txtCantRepeticiones : TextView
     lateinit var  btnNavEjercicio : Button
@@ -44,18 +34,6 @@ class PrevisualizacionEjercicioFragment : Fragment() {
     ): View? {
         v= inflater.inflate(R.layout.fragment_previsualizacion_ejercicio, container, false)
 
-        /*
-        txtEjercicio1= v.findViewById(R.id.lblTxtExercise1)
-        txtEjercicio2= v.findViewById(R.id.lblTxtExercise2)
-        txtEjercicio3= v.findViewById(R.id.lblTxtExercise3)
-        txtEjercicio4= v.findViewById(R.id.lblTxtExercise4)
-
-
-        imagenEj1 =v.findViewById(R.id.imageEjerc1)
-        imagenEj2 =v.findViewById(R.id.imageEjerc2)
-        imagenEj3 =v.findViewById(R.id.imageEjerc3)
-        imagenEj4 =v.findViewById(R.id.imageEjerc4)
-*/
         recyclerEjercicios=v.findViewById(R.id.recyclerEjercicios)
 
         txtCantRepeticiones=v.findViewById(R.id.qtyRepetition)
@@ -78,34 +56,13 @@ class PrevisualizacionEjercicioFragment : Fragment() {
         adapter= EjercicioAdapter(contextSent,repository.ejercicios)
         recyclerEjercicios.layoutManager=LinearLayoutManager(context)
         recyclerEjercicios.adapter=adapter
-      /*
-        txtEjercicio1.text= "Testeando1"
-        txtEjercicio2.text= "Testeando2"
-        txtEjercicio3.text= "Testeando3"
-        txtEjercicio4.text= "Testeando4"
+
+     /*   val decoration = DividerItemDecoration(context,LinearLayoutManager(context).orientation)
+        decoration.setDrawable(ColorDrawable( resources.getColor(R.color.white)));
+        recyclerEjercicios.addItemDecoration(decoration)*/
 
 
 
-        Glide
-            .with(this)
-            .load("https://media.istockphoto.com/id/578104104/es/vector/paso-a-la-instrucci%C3%B3n-en-push-up.jpg?s=612x612&w=0&k=20&c=RNxtdjWZVPjCdk6dBd4wlgNVX7qB6cPFoakeb1Mux8c=")
-            .into(imagenEj1);
-
-        Glide
-            .with(this)
-            .load("https://media.istockphoto.com/id/578104104/es/vector/paso-a-la-instrucci%C3%B3n-en-push-up.jpg?s=612x612&w=0&k=20&c=RNxtdjWZVPjCdk6dBd4wlgNVX7qB6cPFoakeb1Mux8c=")
-            .into(imagenEj2);
-
-        Glide
-            .with(this)
-            .load("https://media.istockphoto.com/id/578104104/es/vector/paso-a-la-instrucci%C3%B3n-en-push-up.jpg?s=612x612&w=0&k=20&c=RNxtdjWZVPjCdk6dBd4wlgNVX7qB6cPFoakeb1Mux8c=")
-            .into(imagenEj3);
-
-        Glide
-            .with(this)
-            .load("https://media.istockphoto.com/id/578104104/es/vector/paso-a-la-instrucci%C3%B3n-en-push-up.jpg?s=612x612&w=0&k=20&c=RNxtdjWZVPjCdk6dBd4wlgNVX7qB6cPFoakeb1Mux8c=")
-            .into(imagenEj4);
-*/
     }
 
 
