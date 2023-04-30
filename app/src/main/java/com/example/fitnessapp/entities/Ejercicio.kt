93f3d95d5af45ca8b06127eceec5a2b74e4b29d1
Mariano <marianotoppino@gmail.com>
2023-04-30 18:27:34 -0300

    Se agrega entidad Ejercicio

--- app/src/main/java/com/example/fitnessapp/entities/Ejercicio.kt
+++ app/src/main/java/com/example/fitnessapp/entities/Ejercicio.kt
@@ -3,5 +3,10 @@ package com.example.fitnessapp.entities
 data class Ejercicio(
     var id: Int,
     var image: String,
-    var description :String
+    var description :String,
+    var cantidad: Int,
+    var paraBajarPeso: Int,
+    var paraGanarPeso: Int,
+    var grupoMuscular: String,
+    var subGrupoMuscular: String
 )
