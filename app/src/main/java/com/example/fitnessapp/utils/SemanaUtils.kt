92bc3250353550a7014701a10414a2010882e388
Mariano <marianotoppino@gmail.com>
2023-05-06 01:50:59 -0300

    Se agrega clase utils

---
+++ app/src/main/java/com/example/fitnessapp/utils/SemanaUtils.kt
@@ -0,0 +1,59 @@
+package com.example.fitnessapp.utils
+
+import com.example.fitnessapp.entities.Rutina
+import com.example.fitnessapp.entities.Semana
+import java.text.SimpleDateFormat
+import java.util.*
+
+class SemanaUtils {
+
+    val dateFormat = SimpleDateFormat("dd-MMMM-yyyy ")
+    var dateFormayDay: SimpleDateFormat = SimpleDateFormat("EEEE")
+
+
+    fun getFechaActual() : String {
+
+        return dateFormat.format(Date())
+    }
+
+    fun getFechaSemanaFormateada(semana:Semana ) : String {
+
+        return dateFormat.format(Date (semana.fechaInicio.time))
+    }
+
+    /**Retorna -1 si la semana esta completa
+     * o el numero de posicion de la rutina si la semana esta incompleta
+     *
+     */
+    fun isSemanaCompleta( semana :Semana):Int{
+
+        var contador :Int =0
+        var posicion :Int=-1
+        var semanaIncompleta : Boolean= false
+
+        while(contador<semana.rutinas.size && !semanaIncompleta  ){
+
+            if(isRutinaDescansoOrFinalizada(semana.rutinas[contador])){
+                contador ++
+            }else{
+                semanaIncompleta=true
+                posicion=contador
+            }
+        }
+       return posicion
+    }
+
+    fun isRutinaDescansoOrFinalizada(rutina : Rutina): Boolean{
+
+          return rutina.esDescanso || rutina.finalizada
+
+    }
+
+    /**  Monday is 1, Sunday is 7. */
+    fun obtenerDia(date: Date) :Int{
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        return calendar.get(Calendar.DAY_OF_WEEK)
+    }
+
+}
