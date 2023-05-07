a04adf9b5c5d35383eb416b2f494ec1451c54abb
Jatzso <91919512+Jatzso@users.noreply.github.com>
2023-05-07 18:05:25 -0300

    Pantalla entrenamiento Home

--- app/src/main/java/com/example/fitnessapp/fragments/EntrenamientoHomeFragment.kt
+++ app/src/main/java/com/example/fitnessapp/fragments/EntrenamientoHomeFragment.kt
@@ -6,22 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnessapp.R
+import com.example.fitnessapp.adapters.SemanaAdapter
 import com.example.fitnessapp.entities.Ejercicio
 import com.example.fitnessapp.entities.EjercicioRepository
 import com.example.fitnessapp.entities.Rutina
+import com.google.android.material.snackbar.Snackbar
 
 class EntrenamientoHomeFragment : Fragment() {
 
     lateinit var v : View
-    lateinit var  btnNavPrev : Button
+    //lateinit var  btnStartExcercise : Button
 
     private lateinit var viewModel: EntrenamientoHomeViewModel
     var repository: EjercicioRepository= EjercicioRepository()
     var rutina: Rutina= Rutina(1,repository.ejercicios,"piernas",false, false,false)
-
+    lateinit var semanaAdapter : SemanaAdapter;
+    lateinit var recyclerView: RecyclerView;
 
 
     override fun onCreateView(
@@ -29,22 +35,31 @@ class EntrenamientoHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_entrenamiento_home, container, false)
-        btnNavPrev =v.findViewById(R.id.btnNavPrevisualization)
+        recyclerView = v.findViewById(R.id.recyclerViewSemana)
+        //btnStartExcercise =v.findViewById(R.id.btn)
         return v
     }
 
     override fun onStart() {
         super.onStart()
 
-        btnNavPrev.setOnClickListener {
+        semanaAdapter = SemanaAdapter(repository.ejercicios){position ->
+            //Snackbar.make(v, "Click en ${repository.ejercicios[position].description}", Snackbar.LENGTH_SHORT).show();
+            val action =EntrenamientoHomeFragmentDirections.actionEntrenamientoHomeFragmentToPrevisualizacionEjercicioFragment(rutina)
+            findNavController().navigate(action)
+        };
+        recyclerView.layoutManager = LinearLayoutManager(context);
+        recyclerView.adapter = semanaAdapter;
 
-
-
+        /*
+        btnStartExcercise.setOnClickListener {
             val action =EntrenamientoHomeFragmentDirections.actionEntrenamientoHomeFragmentToPrevisualizacionEjercicioFragment(rutina)
             findNavController().navigate(action)
 
 
         }
+
+         */
     }
 
 }
